4271d44683684836f7774959a3dcc262f223756f
SirSuPa <noteggwpez@gmail.com>
2024-12-27 11:47:07 +0700

    can run

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import Navbar from './component/Navbar';
+import Footer from './component/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import Courses from './pages/Courses';
